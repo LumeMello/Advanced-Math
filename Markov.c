0e801e541d3a0a9ed411aa1170c8144a27853658
LumeMello <123975458+LumeMello@users.noreply.github.com>
2026-03-04 23:28:28 -0300

    Add files via upload

--- Markov.c
+++ Markov.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool equals(double *v1, double *v2, int n) {
+    int i, cont = 1;
+    for(i=0; i < n; i++) {
+        if (v1[i] != v2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 
 int main()
@@ -9,7 +21,7 @@ int main()
     scanf("%d", &n);
     
     double matrix[n][n];
-    double step[n], soma[n];
+    double step[n], soma[n], lastStep[n];
     
     for(int p = 0; p < n; p++){
         printf("Complete the adjacency matrix line %d",p);
@@ -22,12 +34,12 @@ int main()
         if(p==0){
             step[p] = 1;
         }
+        lastStep[p]=1;
     }
     
     printf("Starting from the first state we have:\n");
     
-    for(int i = 1; i < 1001; i++){
-        
+    do{
         for(int j = 0; j < n; j++){
             soma[j] = 0;
             for(int k = 0; k < n; k++){
@@ -36,12 +48,13 @@ int main()
         }
         
         for(int w = 0; w < n; w++){
+            lastStep[w] = step[w];
             step[w] = soma[w];
             printf("%.4f  ", step[w]);
         }
         printf("\n");
         
-    }
+    }while(!equals(lastStep,step,n));
     
     return 0;
 }
