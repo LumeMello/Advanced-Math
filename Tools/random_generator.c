b61f3070f36a3845bf1d41614f0244895ed2111d
LumeMello <123975458+LumeMello@users.noreply.github.com>
2026-05-24 16:37:33 -0300

    Add files via upload

---
+++ Tools/random_generator.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <time.h>
+
+double initial_valeu = 0;
+
+
+double random(){
+    struct tm* ptr;
+    time_t t;
+    t = time(NULL);
+    ptr = localtime(&t);
+    
+    const char *error_message = "Non percentual value\n";
+    
+    if(initial_valeu > 1 || initial_valeu < 0){
+        fprintf(stderr, "%s", error_message);
+        return 0;
+    }
+    for(int i = 0 ; i < (ptr->tm_sec); i++){
+        initial_valeu = 3.9725599 * initial_valeu * ( 1 - initial_valeu );
+    }
+    
+    return initial_valeu;
+}
+
+int rangen(int range_min, int range_max){
+    struct tm* ptr;
+    time_t t;
+    t = time(NULL);
+    ptr = localtime(&t);
+    
+    initial_valeu = (double)(((double)(ptr->tm_sec)) / 59);
+    
+    int final_value;
+    
+    do{
+        initial_valeu = random();
+        final_value = (int)(initial_valeu * range_max);
+    }while(final_value < range_min);
+    
+    return final_value;
+    
+}
+
+int main()
+{
+    
+    printf("%d",rangen(3,100));
+
+    return 0;
+}
