ee5c736301cf15c129266110feaabecb26b53f2e
LumeMello <123975458+LumeMello@users.noreply.github.com>
2026-05-23 22:37:06 -0300

    Add files via upload

--- Algebra/Vector Operations/products.c
+++ Algebra/Vector Operations/products.c
@@ -9,17 +9,22 @@ double dot_product(double vector1[], double vector2[]){
     return result;
 }
 
-double[] cross_product(double vector1[], double vector2[]){
-    double result[3] = {0};
-    result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1]
-    result[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2]
-    result[2] = vector1[0] * vector2[1] - vector1[1] * vector2[]
+double* cross_product(double vector1[], double vector2[], double result[]){
+    result[3];
+    result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+    result[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+    result[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
     return result;
 }
 
 int main()
 {
-    printf("Hello World");
+    double vector1[3] = {4,2,1};
+    double vector2[3] = {2,2,2};
+    printf("%lf\n",dot_product(vector1,vector2));
+    double *result;
+    result= cross_product(vector1,vector2, result);
+    printf("%lf %lf %lf",result[0],result[1],result[2]);
 
     return 0;
 }
