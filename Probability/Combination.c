f1245b283b530f1668b9778234db6d9d0484b722
LumeMello <123975458+LumeMello@users.noreply.github.com>
2026-06-02 19:55:16 -0300

    Add files via upload

---
+++ Probability/Combination.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define MAX 20
+
+long long factorial_memorization(int n, long long series[]){
+    if(n == 0 || n == 1){
+        return series[n] = 1;
+    }
+    if (series[n] != 0) {
+        return series[n];
+    }
+    
+    return series[n] = n * factorial_memorization(n - 1, series);
+}
+
+long long factorial_tabulation(int n, long long series[]){
+    for(int i = 0; i <= n; i++){
+        if(i == 0 || i == 1){
+            series[i] = 1;
+        }else{
+            if(series[i] == 0){
+                series[i] = i * series[i - 1];
+            }
+        }
+        
+    }
+    
+    return series[n];
+}
+
+long long combination(int n, int r, long long series[]){
+    if (r > n){
+        return 0;
+    }
+    if(r == 0 || r == n){
+        return 1;
+    }
+    
+    return factorial_memorization(n,series)/( factorial_memorization(n-r,series) * factorial_memorization(r, series) );
+}
+
+int main()
+{
+    long long series[MAX] = {0};
+    
+    printf("%lld", combination(18,4,series));
+    
+    return 0;
+}
+
